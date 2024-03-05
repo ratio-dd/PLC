d59091b9677b07be59ec2c3b75259d3df1d53091
ratio <ratio_dd@163.com>
2024-03-05 21:29:12 +0800

    Add burdRate

--- src/SerialPortManager/serialportwidget.cpp
+++ src/SerialPortManager/serialportwidget.cpp
@@ -7,7 +7,14 @@ SerialPortWidget::SerialPortWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::SerialPortWidget) {
     ui->setupUi(this);
+
+    QStringList baudRate = {"300", "600", "1200", "2400", "4800", "9600", "19200", "38400", "43000", "56000", "57600", "115200"};
+
+    ui->BaudRateBox->addItems(baudRate);
+    ui->BaudRateBox->setCurrentIndex(5);
+
 }
+
 
 SerialPortWidget::~SerialPortWidget() {
     delete ui;
